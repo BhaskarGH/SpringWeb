aa14dbc1884a6704480c87e819ff1e40709fdc92
BhaskarGH <bbhaskar.bg@gmail.com>
2024-10-14 17:04:23 +0530

    XSS fix

--- src/main/java/com/bh/SpringWeb/controllers/TestController.java
+++ src/main/java/com/bh/SpringWeb/controllers/TestController.java
@@ -1,5 +1,6 @@
 package com.bh.SpringWeb.controllers;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -20,7 +21,7 @@ public class TestController {
     public String getData(@PathVariable String id){
         String data = webClient.build().get().uri("https://jsonplaceholder.typicode.com/posts").retrieve().bodyToMono(String.class).block();
         System.out.println(data);
-        return id;
+        return StringEscapeUtils.escapeHtml4(id);
     }
 
 
