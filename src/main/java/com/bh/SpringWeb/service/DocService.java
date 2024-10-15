3d93ee0f965589eca65166fa499c16b168b72667
BhaskarGH <bbhaskar.bg@gmail.com>
2024-10-15 11:19:40 +0530

    Snyk Changes

--- src/main/java/com/bh/SpringWeb/service/DocService.java
+++ src/main/java/com/bh/SpringWeb/service/DocService.java
@@ -2,6 +2,8 @@ package com.bh.SpringWeb.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface DocService {
-    public String upload(MultipartFile multipartFile);
+    public String upload(MultipartFile multipartFile) throws IOException;
 }
--- src/main/java/com/bh/SpringWeb/service/DocServiceImpl.java
+++ src/main/java/com/bh/SpringWeb/service/DocServiceImpl.java
@@ -1,11 +1,18 @@
 package com.bh.SpringWeb.service;
 
+import io.netty.handler.codec.http2.Http2Connection;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 @Service
 public class DocServiceImpl implements DocService{
-    public String upload(MultipartFile multipartFile){
+    public String upload(MultipartFile multipartFile) throws IOException {
+        OutputStream outputStream = new  ByteArrayOutputStream(1);
+        outputStream.write(multipartFile.getBytes());
         return "test";
     }
 }
